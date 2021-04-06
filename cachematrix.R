3db317ae3f4df0a521bfbb7dc20e13b4707523d1
VincentWysocki <vinwysocki@gmail.com>
2021-04-05 23:55:41 -0600

    getting ready to push an example cachematrixfile

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#makeCacheMatrix essentially takes in a matrix and provides this matrix to cacheSolve, if it has not already been solved. CacheSolve is able 
+# to access makeCacheMatrix's functions through a passed list. These functions are use to update "m" which is the indicator of a cached matrix..ect.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {                      #Set makes sure that "m"is null the first time makeCacheMatrix is called or a new matrix is provided. 
+                x <<- y                          
+                m <<- NULL
+        }
+        get <- function() x                       #Get passes the matrix to cacheSolve when called
+        
+        setMatrix <- function(newMatrix) m <<- newMatrix #Set revolves m's NULL status so that the same matrix is noted "solved" twice. The operator 
+                                                         # "<<-" is used to update "m" which was changed in cacheSolve.
+        
+        getMatrix<- function() m                  #getMatrix allows us to check for "m's" status. NULL means no matrix is cached   
+                                                
+        list(set = set, get = get,                #List provides a list of the function used in cacheSolve
+             setMatrix = setMatrix,
+             getMatrix = getMatrix)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getMatrix()                        #check if a matrix is cached before the matrix provided is retrieved and solved  
+        if(!is.null(m)) {                         #if "m" is not NULL our matrix can be reloaded 
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()                           #looks like our matrix is not solved, so lets get the new data
+        m <- solve(data, ...)                     #time to crunch some numbers 
+        x$setMatrix(m)                            #deliver "m" to setMatrix by "<<-" operator 
+        m                                         #return matrix 
 }
+
+
+cacheSolve(a)                                     #we call makeCacheMatrix through cacheSolve and a provided matrix 
+
+a <- makeCacheMatrix(m1) #matrix input for makeCacheMatrix
+m1 <- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2)
+
+
